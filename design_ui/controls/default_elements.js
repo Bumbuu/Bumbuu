bf23ae82e6910d91d60117b526ef92ba27cf3d3b
Prince781 <Prince781>
2013-05-16 11:28:00 -0400

    Adding changes.

---
+++ design_ui/controls/default_elements.js
@@ -0,0 +1,19 @@
+/*****
+ * Default user elements for Bumbuu...
+ * 2013 Bumbuu
+******/
+
+function initiate_default_elements() {
+	/* This function simply initiates certain mouse
+	   event captures based on the properties of 
+	   each standard user control. */
+	for (var slider in document.getElementsByClassName("bumbuu_slider")) {
+		for (var handle in slider.getElementsByClassName("bumbuu_slider_handle"))
+			handle.addEventListener("mousedown", function() {
+				slider.addEventListener("mousemove", function(event) {
+					var pos_x = (event.clientX || event.pageX);
+					var pos_y = (event.clientY || event.pageY);
+				});
+			});
+	}
+}
