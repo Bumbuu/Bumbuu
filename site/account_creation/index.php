8dc93cf3f15649cb4854d50b43bc29da29df4d25
Prince781 <princetonf@optonline.net>
2013-07-22 12:30:01 -0400

    Account creation.

--- site/account_creation/index.php
+++ site/account_creation/index.php
@@ -1,5 +1,25 @@
 <?php
 //the general account creation script that conducts SQL queries to update the DB with a new user
-header("Cache-Control: no-store, no-cache, must-revalidate");
-
+if ($_SERVER['REMOTE_ADDR'] !== $_SERVER['SERVER_ADDR']) {
+	//prevent remote POST requests
+	header("No remote access allowed.", true, 400);
+	exit;
+}
+if (isset($_GET['create'])) {
+	header("Cache-Control: no-store, no-cache, must-revalidate");
+	$xml_data = simplexml_load_file("../server_info/server.conf.xml");
+	$db_passwords = array();
+	//populate array with user, password information
+	foreach ($xml_data->children() as $user)
+		foreach ($user->children() as $attr)
+			if ($pass->getName() == 'password')
+				$db_passwords[$user] = $pass;
+	$username = $_POST['username'] or die("No username specified.");
+	$password = $_POST['password'] or die("No password specified.");
+	$email = $_POST['email'] or die("No email specified.");
+	$country = $_POST['country'] or die("No country specified.");
+	$gender = $_POST['gender'] or die("No gender information specified.");
+} else {
+	
+}
 ?>
