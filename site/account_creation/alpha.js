a152c86fea8b8b117492ef3d72d6c33680f131d6
Prince781 <princetonf@optonline.net>
2013-08-01 06:57:42 -0400

    Update.

--- site/account_creation/alpha.js
+++ site/account_creation/alpha.js
@@ -3,6 +3,13 @@
 signup_process = new function() {
 	var _this = this;
 	var current = 1, step_counters, has_initiated = false;
+	var valid = {
+		username: false,
+		firstname: false,
+		lastname: false,
+		email: false,
+		gender: false
+	};
 	this.debug = false; 
 	this.init = function() {
 		if (has_initiated) return false;
@@ -16,9 +23,31 @@ signup_process = new function() {
 					console.log("Scrolling to y-coordinate "+(n*0.8*window.innerHeight)+".");
 			});
 		if (_this.debug) console.log("Signup process has initialized.");
+		$_("input#signup_email").blur(function() {
+			_this.validate(1);
+		});
 	};
-	this.update = function() {
-		
+	this.validate = function(step) {
+		switch (true) {
+		case (step > 2):
+		case (step > 1):
+		case (step > 0):
+			//step 1 validation
+			var username_i = $_("#signup_username");
+			var firstname_i = $_("#signup_firstname");
+			var lastname_i = $_("#signup_lastname");
+			var email_i = $_("#signup_email");
+			var gender_i = $_("#signup_gender");
+			
+			//valid.username = (TODO: AJAX)
+			valid.firstname = /^([^_\W]+)$/.test(firstname_i.value());
+			valid.lastname = /^([^_\W]+)$/.test(lastname_i.value());
+			valid.email = /\w+\@\w+\.\w+/.test(email_i.value());
+			valid.gender = gender_i.value() !== "";
+			for (var f in valid)
+				console.log(f+" validity: "+valid[f]);
+			break;
+		}
 	};
 };
 
