d3052ac1e89b501cdd326ddc93e2bd1755eea115
Prince781 <princetonf@optonline.net>
2013-08-03 02:50:58 -0400

    AJAX validation, notifications, etc.

--- site/account_creation/alpha.js
+++ site/account_creation/alpha.js
@@ -2,7 +2,7 @@
 
 signup_process = new function() {
 	var _this = this;
-	var current = 1, step_counters, has_initiated = false;
+	var current = 1, step_counters, has_initiated = false, timers = {};
 	var valid = {
 		username: false,
 		firstname: false,
@@ -14,42 +14,113 @@ signup_process = new function() {
 	this.init = function() {
 		if (has_initiated) return false;
 		else has_initiated = true;
+		$_("#signup_username, #signup_firstname, #signup_lastname, #signup_email").value("");
 		step_counters = $_(".step_counter").div;
 		for (var i=1; i<step_counters.length; i++) {
 			$_(step_counters[i]).attr("active", true);
 			$_(step_counters[i]).click(function() {
 				var n = parseInt($_(this).html())-1;
+				if (!_this.validate(n))
+					return _this.notify("warning", "Failed to validate!");
 				$_(window).effects.scrollTo("y", n*0.8*window.innerHeight, 1200);
 				if (_this.debug)
 					console.log("Scrolling to y-coordinate "+(n*0.8*window.innerHeight)+".");
 			});
 		}
 		if (_this.debug) console.log("Signup process has initialized.");
-		$_("input#signup_email").blur(function() {
-			_this.validate(1);
+		$_("#signup_username").keyup(function() {
+			_this.validate("username");
+		});
+		$_("#signup_firstname").keyup(function() {
+			_this.validate("firstname");
+		});
+		$_("#signup_lastname").keyup(function() {
+			_this.validate("lastname");
+		});
+		$_("#signup_email").keyup(function() {
+			_this.validate("email");
 		});
 	};
 	this.validate = function(step) {
-		switch (true) {
-		case (step > 2):
-		case (step > 1):
-		case (step > 0):
-			//step 1 validation
-			var username_i = $_("#signup_username");
-			var firstname_i = $_("#signup_firstname");
-			var lastname_i = $_("#signup_lastname");
-			var email_i = $_("#signup_email");
-			var gender_i = $_("#signup_gender");
-			
-			//valid.username = (TODO: AJAX)
-			valid.firstname = /^([^_\W]+)$/.test(firstname_i.value());
-			valid.lastname = /^([^_\W]+)$/.test(lastname_i.value());
-			valid.email = /\w+\@\w+\.\w+/.test(email_i.value());
-			valid.gender = gender_i.value() !== "";
-			for (var f in valid)
-				console.log(f+" validity: "+valid[f]);
-			break;
-		}
+		validate = {
+			username: function() {
+				var d = $_("#signup_username");
+				valid.username = /^([^\W]+)$/.test(d.value());
+				if (valid.username)
+					$_.req({
+						url: "index.php?validate",
+						method: "post",
+						data: "username="+encodeURIComponent(d.value()),
+						headers: ["Content-Type", "application/x-www-form-urlencoded"],
+						readystatechange: function(ajax) {
+							if (ajax.readyState !== 4) return;
+							valid.username = (ajax.responseText == "valid");
+							if (!valid.username) {
+								_this.notify("warning", "That username is already taken.");
+								$_("#signup_username_label").attr("type", "invalid");
+							} else $_("#signup_username_label").attr("type", "valid");						
+						}
+					});
+				else {
+					_this.notify("warning", "Improper username. Only alphanumeric characters are allowed.");
+					$_("#signup_username_label").attr("type", "invalid");
+				}
+			},
+			email: function() {
+				var d = $_("#signup_email");
+				valid.email = /^(\w+\@\w+\.\w+)$/.test(d.value());
+				if (valid.email)
+					$_.req({
+						url: "index.php?validate",
+						method: "post",
+						data: "email="+encodeURIComponent(d.value()),
+						headers: ["Content-Type", "application/x-www-form-urlencoded"],
+						readystatechange: function(ajax) {
+							if (ajax.readyState !== 4) return;
+							valid.email = (ajax.responseText == "valid");
+							d.attr("validity", valid.email ? "valid" : "invalid");
+							if (!valid.email) _this.notify("warning", "That email is already taken.");
+						}
+					});
+				else d.attr("validity", valid.email ? "valid" : "invalid");
+			},
+			firstname: function() {
+				var d = $_("#signup_firstname");
+				valid.firstname = /^([^_\W]+)$/.test(d.value());
+				d.attr("validity", valid.firstname ? "valid" : "invalid");
+			},
+			lastname: function() {
+				var d = $_("#signup_lastname");
+				valid.lastname = /^([^_\W]+)$/.test(d.value());
+				d.attr("validity", valid.lastname ? "valid" : "invalid");
+			}
+		};
+		if (typeof step == "number")
+			switch (true) {
+			case (step > 2):
+			case (step > 1):
+			case (step > 0):
+				//step 1 validation
+				validate.username();
+				validate.email();
+				validate.firstname();
+				validate.lastname();
+				break;
+			}
+		else if (step in validate) 
+			validate[step]();
+	};
+	this.notify = function(type, message) {
+		if (type in timers) clearTimeout(timers["notif"]);
+		var d = $_($_(".bumbuu_intext_notif").div[0]);
+		d.attr("type", type);
+		d.html(encodeURI(message).replace(/\%20/g, " "));
+		d.effects.fadeTo(100, 618, function() {
+			timers["notif"] = setTimeout(function() {
+				$_($_(".bumbuu_intext_notif").div[0]).effects.fadeTo(0, 618);
+			}, 7000);
+		});
+		return {fail: false, success: true}[type];
 	};
 };
 
--- site/account_creation/index.php
+++ site/account_creation/index.php
@@ -1,30 +1,46 @@
 <?php
 //the general account creation script that conducts SQL queries to update the DB with a new user
-if ($_SERVER['REMOTE_ADDR'] !== $_SERVER['SERVER_ADDR']) {
+/*if ($_SERVER['HTTP_REFERER'] !== $_SERVER['HTTP_HOST']) {
 	//prevent remote POST/GET requests
 	header("No remote access allowed.", true, 400);
 	exit;
-}
-if (isset($_GET['create'])):
+} TODO: fix */
+$xml_data = simplexml_load_file("../server_info/server.conf.xml");
+$db_passwords = array();
+//populate array with user, password iznformation
+foreach ($xml_data->children() as $user)
+	foreach ($user->children() as $user_attr)
+		if ($user_attr->getName() == 'password')
+			$db_passwords[((string)$user['name'])] = $user_attr;
+if (isset($_GET['create'])) {
 	header("Cache-Control: no-store, no-cache, must-revalidate");
-	$xml_data = simplexml_load_file("../server_info/server.conf.xml");
-	$db_passwords = array();
-	//populate array with user, password iznformation
-	foreach ($xml_data->children() as $user)
-		foreach ($user->children() as $attr)
-			if ($pass->getName() == 'password')
-				$db_passwords[$user] = $pass;
 	$username = $_POST['username'] or die("No username specified.");
 	$password = $_POST['password'] or die("No password specified.");
 	$email = $_POST['email'] or die("No email specified.");
 	$country = $_POST['country'] or die("No country specified.");
 	$gender = $_POST['gender'] or die("No gender information specified.");
-else: //display normal page
+} elseif (isset($_GET['validate'])) {
+	header("Cache-Control: no-store, no-cache, must-revalidate");
+	header("Content-Type: text/plain");
+	$con = mysql_connect("localhost","bumbuuco_usrdata",$db_passwords["bumbuuco_usrdata"]) or die("Unable to connect to SQL server");
+	mysql_select_db("bumbuuco_users", $con) or die("Unable to SELECT DB");
+	if (isset($_POST['username']))
+		$results = mysql_query(
+			sprintf("SELECT * FROM userlist where Username='%s' LIMIT 1", mysql_real_escape_string($_POST['username']))
+		) or die("SQL error.");
+	elseif (isset($_POST['email']))
+		$results = mysql_query(
+			sprintf("SELECT * FROM userlist where Email='%s' LIMIT 1", mysql_real_escape_string($_POST['email']))
+		) or die("SQL error.");
+	else exit("No options provided.");
+	if (mysql_num_rows($results) > 0) exit("invalid");
+	elseif (mysql_num_rows($results) == 0) exit("valid");
+} else { //display normal page
 ?>
 
-<!--webpage goes here-->
+<!-- main page goes here -->
 <!DOCTYPE html>
 <html lang="en">
 </html>
 
-<?php endif; ?>
+<?php } ?>
