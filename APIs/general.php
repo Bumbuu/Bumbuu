d9f8479cf713a33b676f85e8a1e1dc0980c9de14
Prince781 <princetonf@optonline.net>
2013-08-10 05:18:54 -0400

    Adding general PHP functions; fixing hash parameters.

---
+++ APIs/general.php
@@ -0,0 +1,12 @@
+<?php
+//general.php - general functions for use across Bumbuu
+
+class Bumbuu {
+	//security
+	public static function generate_salt() {
+		//default salt length for Bumbuu is 20 characters
+		return substr(hash("sha256", mt_srand(mt_rand())), 0, 20);
+	}
+}
+
+?>
