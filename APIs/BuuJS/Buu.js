05dfd405cf25eb500450fdd0936ccfc322e3e265
Prince781 <princetonf@optonline.net>
2013-09-26 11:17:27 -0400

    Update.

--- APIs/BuuJS/Buu.js
+++ APIs/BuuJS/Buu.js
@@ -15,6 +15,41 @@
 const BuuJS = new function() {
 	var that = this;
 	this.get = function(en) {
-		return document.
+		return document.querySelectorAll(en);
 	};
+	this.animate = function(anim_type, end_state, length, ease_func) {
+		//returns new animation
+		var anim_this = this;
+		var timer = 0, interval = 0;
+		var finish_function = new Function();
+		
+		function do_action() {
+			
+		}
+		
+		//basic start/stop
+		this.start = function() {
+			interval = setInterval(do_action, 1);
+			return anim_this;
+		};
+		this.stop = function() {
+			clearInterval(interval);
+			return anim_this;
+		};
+		this.onFinish = function(noitcnuf) {
+			finish_function = noitcnuf;
+			return anim_this;
+		};
+	};
+	this.animations = {
+		show: function() {
+			
+		}
+	};
+	function BuuJSItem(el) {
+		this.el = el;
+		this.anim = function(anim_type, end, length, ease_func) {
+			return new that.animate(anim_type, start, end, length, ease_func);
+		};
+	}
 };
